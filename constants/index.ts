7b4d741f3302ab3ce33443fe86301debba2d6e43
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-17 13:11:55 +0000

    constants data added

---
+++ constants/index.ts
@@ -0,0 +1,107 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import carhubImg from "@/public/carhub.png";
+import realtorImg from "@/public/realtor.png";
+import ecommerceImg from "@/public/ecommerce.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Passed 10th",
+    location: "Sitapur, IN",
+    description:
+      "Completed my high school curriculum, gaining a solid foundation in various subjects and essential skills.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021",
+  },
+  {
+    title: "React.js Bootcamp",
+    location: "Lucknow, IN",
+    description:
+      "Successfully completed an intensive 1-month React.js Developer Bootcamp. Acquired expertise in React components, state management with Redux, and interacting with RESTful APIs",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022",
+  },
+  {
+    title: "Freelancer React.js Developer",
+    location: "Bangalore, IN",
+    description:
+      "Working as a freelance React.js Developer, I have collaborated with various clients to create responsive and dynamic web applications.",
+    icon: React.createElement(FaReact),
+    date: "2022 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CarHub",
+    description:
+      "A web app that is built using NextJS and enables users to quickly search and obtain information about cars.",
+    tags: ["React", "Next.js", "TypeScript", "Tailwind", "Headless UI"],
+    imageUrl: carhubImg,
+  },
+  {
+    title: "Realtor",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "Chakra UI", "Next.js", "Framer Motion", "React Icons"],
+    imageUrl: realtorImg,
+  },
+  {
+    title: "E commerce Store",
+    description:
+      "A NextJS-based eCommerce store that allows users to order different products. It supports real-time payments using Stripe.",
+    tags: ["React", "Next.js", "Sanity", "React Router", "Stripe"],
+    imageUrl: ecommerceImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
