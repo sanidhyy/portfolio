b26458347b1d6a02ac90635ae6c24c93e14fa234
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-18 14:08:05 +0000

    typo fixes

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 
 export const Intro = () => {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -27,7 +27,7 @@ export const Intro = () => {
               height={192}
               quality={95}
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.25rem] border-white shadow-xl"
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
           <motion.span
@@ -46,17 +46,17 @@ export const Intro = () => {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I&apos;m Sanidhya.</span> I&apos;m a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">5+ years</span> of experience. I enjoy
-        building <span className="italic">sites and apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>
-      </motion.p>
+        <b className="font-bold">Hi, I&apos;m Sanidhya.</b> I&apos;m a{" "}
+        <b className="font-bold">full-stack developer</b> with{" "}
+        <b className="font-bold">5+ years</b> of experience. I enjoy building{" "}
+        <b className="italic">sites and apps</b>. My focus is{" "}
+        <b className="underline">React (Next.js)</b>
+      </motion.h1>
 
       <motion.div
         initial={{ opacity: 0, y: 100 }}
