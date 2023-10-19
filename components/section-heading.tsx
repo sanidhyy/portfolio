7c396ed52489690178b30e2ec8cfb908bab0aa95
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-19 11:31:21 +0000

    text centered

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -3,7 +3,11 @@ type SectionHeadingProps = {
 };
 
 const SectionHeading = ({ children }: SectionHeadingProps) => {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+      {children}
+    </h2>
+  );
 };
 
 export default SectionHeading;
