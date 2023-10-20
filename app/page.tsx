3ca79e0590e89536268a194c0cd46565c6f447e8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-20 12:08:26 +0000

    experience section implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/about";
+import Experience from "@/components/experience";
 import { Intro } from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   );
 }
---
+++ components/experience.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import SectionHeading from "./section-heading";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { experiencesData } from "@/constants";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useSectionInView } from "@/lib/hooks";
+
+import "react-vertical-timeline-component/style.min.css";
+
+const Experience = () => {
+  const { ref } = useSectionInView("Experience", 0.5);
+  const { activeSection } = useActiveSectionContext();
+
+  const [isExperienceSectionActive, setIsExperienceSectionActive] =
+    useState(false);
+
+  useEffect((): void => {
+    if (activeSection === "Experience") {
+      setIsExperienceSectionActive(true);
+    }
+  }, [activeSection]);
+  return (
+    <section ref={ref} id="experience">
+      <SectionHeading>My experience</SectionHeading>
+
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((experience, i) => (
+          <React.Fragment key={`experience-${i}`}>
+            <VerticalTimelineElement
+              visible={isExperienceSectionActive}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={experience.date}
+              icon={experience.icon}
+              iconStyle={{
+                background: "#fff",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{experience.title}</h3>
+              <p className="font-normal !mt-0">{experience.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">
+                {experience.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  );
+};
+
+export default Experience;
