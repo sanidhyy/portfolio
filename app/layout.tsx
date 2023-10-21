a58780e1741f9e1eda203ebd825126a89027e377
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-21 14:22:58 +0000

    theme switch btn added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,10 @@ export default function RootLayout({
 
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+
+        <aside>
+          <ThemeSwitch />
+        </aside>
       </body>
     </html>
   );
---
+++ components/theme-switch.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+type Theme = "light" | "dark";
+
+const ThemeSwitch = () => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  return (
+    <button
+      type="button"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
+    </button>
+  );
+};
+
+export default ThemeSwitch;
