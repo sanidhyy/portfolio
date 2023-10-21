84de9cd54f0001881e42db2bfe5f22e65811a65a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-21 15:53:42 +0000

    header flex wrapping fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,13 +3,13 @@ const Footer = () => {
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         &copy; {new Date().getFullYear()}-{(new Date().getFullYear() % 100) + 1}{" "}
-        Sanidhya. All rights reserved.
+        <b>Sanidhya</b>. All rights reserved.
       </small>
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React &amp; Next.js (App Router and Server Actions), Typescript,
-        Tailwind CSS, Framer Motion, EmailJS, React Toaster, React Vertical
-        Timeline &amp; Vercel Hosting
+        <b className="font-semibold">About this website:</b> built with React
+        &amp; Next.js (App Router and Server Actions), Typescript, Tailwind CSS,
+        Framer Motion, EmailJS, React Toaster, React Vertical Timeline &amp;
+        Vercel Hosting
       </p>
     </footer>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
       />
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
