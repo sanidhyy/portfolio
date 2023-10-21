418fa8aa4643be8845f33450771577e36487fb8c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-21 14:00:38 +0000

    footer implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
 
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
---
+++ components/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; {new Date().getFullYear()}-{(new Date().getFullYear() % 100) + 1}{" "}
+        Sanidhya. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        React &amp; Next.js (App Router and Server Actions), Typescript,
+        Tailwind CSS, Framer Motion, EmailJS, React Toaster, React Vertical
+        Timeline &amp; Vercel Hosting
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
