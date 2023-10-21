28579474c099053055d50d8f54c0390d7f47921b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-21 15:42:37 +0000

    responsiveness fixes

--- components/project.tsx
+++ components/project.tsx
@@ -27,8 +27,8 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
       ref={projectRef}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <div className="bg-gray-100 max-w-[42rem] group-even:pl-8 border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] rounded-lg hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <div className="bg-gray-100 max-w-[42rem] sm:group-even:pl-8 border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] rounded-lg hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -49,7 +49,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
           src={imageUrl}
           alt={title}
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem]
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem]
          rounded-t-lg shadow-2xl group-even:right-[initial] 
         group-even:-left-40 group-hover:-translate-x-3
          group-hover:translate-y-3 group-hover:-rotate-2
