e582dbbfdeec890cc82f418d75c22283bb44dfce
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-10-22 04:26:00 +0000

    [pwa] manifest added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const prod = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: prod ? false : true,
+});
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
