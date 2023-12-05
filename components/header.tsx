44da84378a4b13ecf5069f3e2dfe2c6718a4f1ae
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-05 07:14:05 +0000

    styling fixes

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
       />
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-400 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {LINKS.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
@@ -35,7 +35,7 @@ const Header = () => {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-200",
                   {
-                    "text-gray-950 dark:text-gray-100":
+                    "!text-gray-950 font-medium dark:!text-gray-100":
                       activeSection === link.name,
                   }
                 )}
