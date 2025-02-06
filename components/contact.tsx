b07c7144436738d087cfffa553ba379a667fe087
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-02-06 22:18:46 +0530

    refactor: contact submit btn position

--- components/contact.tsx
+++ components/contact.tsx
@@ -213,7 +213,7 @@ const Contact = () => {
         {/* Submit button with conditional rendering for loading state. */}
         <button
           type="submit"
-          className="group flex self-center sm:self-start items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 disabled:scale-100 disabled:bg-opacity-65 dark:bg-white dark:bg-opacity-10"
+          className="group flex self-center items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 disabled:scale-100 disabled:bg-opacity-65 dark:bg-white dark:bg-opacity-10"
           disabled={loading}
         >
           {loading ? (
